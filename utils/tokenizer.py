cf84df1a81d6c55108a991a418160a2d60095b6b
Zi <zyin@chaos2.stanford.edu>
2018-12-17 23:17:52 -0800

    use min_count to control word frequency threshold

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -24,9 +24,14 @@ class SimpleTokenizer():
         return tokenized
 
     #TODO: add min_count, together with n_words to determine if UNK is needed
-    def frequency_count(self, tokenized_data, n_words):
+    def frequency_count(self, tokenized_data, n_words, min_count):
         count = [['UNK', -1]]
+        num_above_threshold = 0
         counter = collections.Counter(tokenized_data)
+        for k, v in counter.items():
+            if v >= min_count:
+                num_above_threshold += 1
+        n_words = min(n_words, num_above_threshold)
         # if more tokens than needed, map the rest to UNK
         if len(counter) > n_words:
             count.extend(collections.Counter(tokenized_data).most_common(n_words - 1))
@@ -52,9 +57,9 @@ class SimpleTokenizer():
         data = [_index(word) for word in tokenized_data]
         return data
     
-    def do_index_data(self, data, n_words=10000):
+    def do_index_data(self, data, n_words=10000, min_count=100):
         """transform data: str into a tokens: list. tokens are mapped to {0, 1, ..., n_words - 1}"""
         self.tokenized = self.tokenize(data)
-        self.dictionary, self.reversed_dictionary = self.frequency_count(self.tokenized, n_words)
+        self.dictionary, self.reversed_dictionary = self.frequency_count(self.tokenized, n_words, min_count)
         self.indexed = self.index(self.tokenized, self.dictionary)
         return self.indexed
